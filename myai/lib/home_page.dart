1bfa11f920663aa060244781bbe92a1163161801
Shadabdullah <shadabdullah101@gmail.com>
2023-04-21 11:14:31 +0530

    some updates

--- myai/lib/home_page.dart
+++ myai/lib/home_page.dart
@@ -15,59 +15,74 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          ClipOval(
-            child: Container(
-              height: 120,
-              width: 120,
-              margin: const EdgeInsets.only(top: 4),
-              color: Pallete.assistantCircleColor,
-              child: Image.asset("assets/images/assitant.png", height: 123),
-            ),
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(top: 4),
+                  decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Container(
+                  height: 123,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage("assets/images/assitant.png"))))
+            ],
           ),
           const SizedBox(height: 30),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            margin: const EdgeInsets.symmetric(horizontal: 45),
-            decoration: BoxDecoration(
+               Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 45).copyWith(top: 30),
+          decoration: BoxDecoration(
               border: Border.all(
                 color: Pallete.borderColor,
               ),
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-              ),
-            ),
-            child: const Text(
-              "Good Morning, what task can I do for you?",
-              style: TextStyle(
-                fontFamily: 'Cera Pro',
-                color: Pallete.mainFontColor,
-                fontSize: 20,
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          const Padding(
-            padding: EdgeInsets.only(left: 30),
+              borderRadius:
+                  BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
             child: Text(
-              'Here are a few features',
+              "Good Morning , What task can I do for you ?",
               style: TextStyle(
-                fontFamily: 'Cera Pro',
-                color: Pallete.mainFontColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 17,
-              ),
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20),
             ),
           ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.only(top: 10, left: 30),
+          alignment: Alignment.centerLeft,
+          child: const Text(
+            'Here are a few features',
+            style: TextStyle(
+              fontFamily: 'Cera Pro',
+              color: Pallete.mainFontColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 17,
+            ),
+          ),
+        ),
           Expanded(
             child: ListView(
               children: const [
-                 FeatureBox(
+                FeatureBox(
                   color: Pallete.firstSuggestionBoxColor,
                   headerText: 'ChatGPT',
                   description:
                       'The intelligent solution for enhanced organization and information management.',
                 ),
-                 FeatureBox(
+                FeatureBox(
                   color: Pallete.secondSuggestionBoxColor,
                   headerText: 'Dall-E',
                   description:
